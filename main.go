5991ab6b2c79b186c1e3eacb6c0447a07328af45
Adam Reuben <adamreuben@bongocloud.co.tz>
2026-05-01 17:51:28 +0300

    fix

--- main.go
+++ main.go
@@ -499,7 +499,7 @@ var functionsDeployCmd = &cobra.Command{
 	},
 }
 
-const CLIAppToken = "eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJkYXRhIjp7ImFwcElkIjoidHdpZ2dhdG9vbHMiLCJhcHBTZWNyZXQiOiI1MjAxYzhmNC1hYjE3LTRmZTQtOTcxZC1lZGMwMzgzOTMwZGMiLCJleHAiOjE3NTg1Mzg1NTR9LCJleHAiOjE3NTg1Mzg1NTR9.wSJM1YnC4VdOGzSUmZ3r8v0uOJGA7g9L2X3fgQkdt6ciafX9SLnVK8zkExjC5arrutD4tRolyeUg-YpTJaJS4mOdxL3LMX8uulnbGUhpEbrawFMyGuStsZ7dgLxFpUxlAHbaQfutRFnoPYZnsjqmhWsgeW44taDe0S7HaypNqJJsNXK21iA-8-bToFKepTbLeKl9jCLfseyyGfrFcuQBXjuhjnJiwfQXFkKeoZ8-aE86fdwidCpbOmEEf9z-XwDwo_QzzbTyQh7Npr0MQOggXlVWF7TRhDqQa4X0EH4_ErmIGZEC9W57gvKiShdZYrhl2VYtgwHP1bd7UeWr6cw-Pw"
+const CLIAppToken = "3D7xSbwYmrslKWEvelWIJfdEFEr"
 
 func main() {
 	home, err := os.UserHomeDir()
